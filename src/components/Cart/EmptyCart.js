b6b371798e1cb07b4be5ada559a0f04a72d20afc
radokham <radokham6@gmail.com>
2019-03-28 13:44:02 +0300

    second commit

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function EmptyCart() {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-10 mx-auto text-center text-title text-capitalize">
+          <h1>Contact me on:  </h1>
+          <h3> Gmail:radokham6@gmail.com</h3>
+          <h3> Phone:+261341327296</h3>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Escalade - Black",
+    img: "img/product-1.png",
+    price: 50000000,
+    company: "Cadillac",
+    info:
+      "les meilleurs services de voitures de luxe, limousines, bus de soirée.Airbag: Conducteur, Airbag: Passager, Alarme antivol, Freins antiblocage, Caméra de recul",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Ferrari F12",
+    img: "img/product-2.png",
+    price: 5000000,
+    company: "GT Ferrari",
+    info:
+      "La F12berlinetta bénéficie de plusieurs innovations technologiques dérivées de l'expérience acquise en F1 afin de se mettre au niveau de ses concurrentes, les coupés V12 tels que la Lamborghini Aventador.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Nissan Qashqai",
+    img: "img/product-3.png",
+    price: 40000000,
+    company: "Japan Nissan",
+    info:
+      "La Nissan Qashqai est un crossover produit par la marque japonaise Nissan. Son nom trouve ses origines dans le mot Kachkaïs: Qashqā'i en persan.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "ASTON-MARTIN",
+    img: "img/product-4.png",
+    price: 60000000,
+    company: "VANTAGE",
+    info:
+      "Une E type 3.8L c'est une histoire et un design extraordinaire, la descendante des D type de course. Faite de la même façon coque + chassis tubulaire. Ce qui lui donne pour l'époque une tenue de route trés saine, les trains roulants sont rêglables, et il y a les jantes rayons larges d'époque en option. Comme toujours c'est la première version (3.8) qui est la plus pure en dessin et la plus demandée donc chère... Ma E OTS 3.8L planchers plats me donne toute satisfaction, entretien simple e.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Cacao d'Ambanja",
+    img: "img/product-5.png",
+    price: 200000,
+    company: "htc",
+    info:
+      "Le cacao est la poudre obtenue après broyage de l'amande des fèves de cacao plates fermentées produites par le cacaoyer. Cette opération permet également d'extraire la graisse que l'on appelle beurre de cacao. La pâte de cacao est quant à elle produite à partir des fèves de cacao fermentées et torréfiées.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Cacao ",
+    img: "img/product-6.png",
+    price: 25000,
+    company: "Madagasikara",
+    info:
+      "	Le cacao a une multitude de bienfaits pour la santé, il ne faut donc pas le supprimer de l'alimentation mais le consommer avec modération. Privilégiez les cacaos en poudre non sucré pour réaliser des boissons et du chocolat avec au moins 60% de cacao. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: " Cacao -Poudre",
+    img: "img/product-7.png",
+    price: 30000,
+    company: "Madagasikara",
+    info:
+      "Le cacao est la poudre soluble obtenue à partir des extraits de la graine du cacaoyer , dite fève de cacao, et qui est utilisée dans l'alimentation. Lors de cette opération, on extrait également la graisse appelée beurre de cacao..",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Cacao",
+    img: "img/product-8.png",
+    price: 250000,
+    company: "Antsiranana",
+    info:
+      "Grâce au cacao, le nord de Mada a échappé à la déforestation catastrophique qui continue de ravager la Grande Île de l’océan Indien. Les plaines du nord, et surtout du nord-ouest, sont recouvertes à perte de vue de cacaoyers qui peuvent atteindre les douze mètres....",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
